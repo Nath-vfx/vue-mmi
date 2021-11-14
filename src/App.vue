a841ec7f57df35f2f815e26b648332867eff60d1
Nathan Van Bignoot <nathan.van-bignoot@outlook.fr>
2021-11-14 22:09:52 +0100

    add: operational less + begin of nav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header>
+      <nav id="nav">
+        <div id="brand">
+          <router-link to="/">
+            <img src="images/Logo%20MMi-black.svg" alt="Logo du département MMI de Montbéliard">
+          </router-link>
+        </div>
+        <ul>
+          <li>
+            <a href="#">Département</a>
+          </li>
+          <li>
+            <a href="#">BUT MMi</a>
+          </li>
+          <li>
+            <a href="#">Mobilité Internationale</a>
+          </li>
+        </ul>
+        <div id="extras" class="menu-deroulant">
+          <a href="#">Extras</a>
+          <ul class="sous-menu">
+            <li>
+              <a href="#">Contact</a>
+              <a href="#">Espace Pro</a>
+              <a href="#">Actualités</a>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+    </header>
+
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-
+@import "assets/style/style.css";
 
 </style>
