88643bfb1c7e8885242090fd39488eaa8110a6ca
Elora.PERRIN <elo6tm@outlook.fr>
2021-12-10 08:59:58 +0100

    Add contenus Actualités

--- src/views/Actualites.vue
+++ src/views/Actualites.vue
@@ -1,13 +1,48 @@
 <template>
-<div>
-  <h1>{{msg}}</h1>
-</div>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <div class="main">
+      <img src="" alt="">
+      <h3>Journée Portes Ouvertes 2022</h3>
+      <time>29 Janvier 2022</time>
+    </div>
+
+    <aside>
+      <img src="" alt="">
+      <h3>Remise des diplômes</h3>
+      <time>29 Juin 2022</time>
+    </aside>
+
+    <h2>Dernières nouvelles</h2>
+
+    <ul class="articles">
+      <li>
+        <img src="#" alt="">
+        <time>7 Novembre 2021</time>
+        <h4>Lorem ipsum</h4>
+        <p>Lorem ipsum</p>
+      </li>
+      <li>
+        <img src="#" alt="">
+        <time>8 Novembre 2021</time>
+        <h4>Lorem ipsum</h4>
+        <p>Lorem ipsum</p>
+      </li>
+      <li>
+        <img src="#" alt="">
+        <time>9 Novembre 2021</time>
+        <h4>Lorem ipsum</h4>
+        <p>Lorem ipsum</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Actualites",
-  data () {
+  data() {
     return {
       msg: 'Page actualités'
     }
