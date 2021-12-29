d031c047ef7800e6ee22ec2866c7ab0105f8abfe
Elora.PERRIN <elo6tm@outlook.fr>
2021-12-29 15:02:21 +0100

    Mentions + politique CSS and HTML
    Tous les contenus mis, CSS à régler

--- src/views/MentionsLegales.vue
+++ src/views/MentionsLegales.vue
@@ -1,13 +1,71 @@
 <template>
-<div>
-  <h1>{{msg}}</h1>
-</div>
+  <div>
+    <h1>Mentions<br><span>Légales</span></h1>
+    <p>Cette page décrit les mentions légales qui s'appliquent à tout internaute visitant ce site. En le consultant,
+      vous vous engagez sans réserve à les respecter. Les mentions légales pouvant être modifiées à tout moment et sans
+      préavis, nous vous engageons à les consulter régulièrement.</p>
+    <div>
+      <h2>Responsable éditorial et responsable légal</h2>
+      <p>Département des Métiers du Multimédia et de l’Internet <br>
+        Représenté(e) par : Alain Lamboux Durand<br>
+        E-mail : alain.lamboux-durand@edu.univ-fcomte.fr
+      </p>
+    </div>
+    <div>
+      <h2>Éditeur du site</h2>
+      <p>Département des Métiers du Multimédia et de l’Internet, Formation BUT MMI
+        <br>Représenté(e) par les étudiants : </p>
+      <p> <span>Nathan Van Bignoot :</span> Chef de projet et Développeur web
+        <br><span>Elora Perrin :</span> Responsable de la Communication
+        <br><span>Tania André :</span> Responsable du Design
+        <br><span>Eva Bourquin :</span> Responsable du Design
+        <br>4 Place Tharradin, 25200 Montbéliard
+        <br>mmi@univ-fcomte.fr
+        <br>Tuteur légal des étudiants : Xavier Senente
+      </p>
+    </div>
+    <div>
+      <h2>Hébergeur</h2>
+      <p>Centre de Ressources Informatiques (CRI), Université de Franche-Comte
+        <br>Représenté(e) par : Claude Girard (IUT Belfort-Montbéliard - site Montbéliard)
+        <br>Tél : 03 81 99 46 08
+        <br>E-mail : claude.girard@univ-fcomte.fr
+      </p>
+    </div>
+    <div>
+      <h2>Conditions d'utilisation</h2>
+      <p>Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès et de rectification aux données vous concernant par simple demande mail, écrite ou téléphonique. Le département MMI et les élèves en charge du projet ne peuvent être tenus responsables du contenu du site. Ce site est exploité par le département MMI de Montbéliard. La présentation et chacun des éléments, y compris les marques, logos et noms de domaine, apparaissant sur le site https://mmimontbeliard.com sont protégés par les lois en vigueur sur la propriété intellectuelle, et appartiennent à l’IUT de Belfort-Montbéliard, ou font l'objet d'une autorisation d'utilisation. Aucun élément composant le site ne peut être copié, reproduit, modifié, réédité, chargé, dénaturé, transmis ou distribué de quelque manière que ce soit, sous quelque support que ce soit, de façon partielle ou intégrale, sans l'autorisation écrite et préalable du département MMI à l'exception d'une stricte utilisation pour les besoins de la presse et sous réserve du respect des droits de propriété intellectuelle et de tout autre droit de propriété dont il est fait mention. Seule la copie à usage privé est autorisée pour votre usage personnel, privé et non-commercial, sur votre ordinateur personnel. Toute utilisation autorisée des éléments composant ou figurant dans le site doit se faire sans dénaturation, modification ou altération de quelque manière que ce soit. Le département MMI se réserve le droit de poursuivre tout acte de contrefaçon de ses droits de propriété intellectuelle. Le présent Contrat est soumis au droit Français. Le présent Contrat s'applique dans le monde entier, et seule sa version en langue française fait foi en vue de son interprétation.
+      </p>
+    </div>
+    <div>
+      <h2>Graphisme, photos et illustrations</h2>
+      <p>Le graphisme (incluant le webdesign et le logo) a été réalisé par Nathan Van Bignoot, Tania André et Eva Bourquin.
+        <br>Les photographies et la vidéo ont été réalisées par Nathan Van Bignoot et Elora Perrin. Les illustrations et visuels ont téléchargés sur les banques d’images libres de droits Freepik, Pixabay, Unsplash, Flaticon etc.
+      </p>
+    </div>
+    <div>
+      <h2>Directeur de publication</h2>
+      <p>Alain Lamboux Durand, chef du département, sous l’autorité de David Markezic, directeur de <strong>l’IUT de Belfort-Montbéliard</strong>, et de Macha Woronoff, présidente de <strong>l’Université de Franche-Comté</strong>.
+      </p>
+    </div>
+    <div>
+      <h2>Droits d'auteurs</h2>
+      <h3>Loi n°78-17</h3>
+      <p>Conformément à l’article 34 de la loi informatique et Libertés du 6 janvier 1978, vous disposez d’un droit d’accès aux informations vous concernant. Vous pouvez également modifier ou supprimer ces informations. Il vous suffit de nous contacter à l’adresse suivante : mmi@univ-fcomte.fr.
+
+        <br>Les fichiers résultants ne sont jamais transmis à des tiers. En particulier, nous ne commercialisons aucun fichier d’adresses ou d’emails.
+      </p>
+      <h3>Droits d'auteurs et Copyright</h3>
+      <p>L’ensemble de ce site relève de la législation française et internationale sur le droit d’auteur et la propriété intellectuelle. Tous les droits de reproduction sont réservés, y compris pour les documents téléchargeables et les représentations iconographiques et photographiques. La reproduction de tout ou partie de ce site sur un support électronique quel qu’il soit est formellement interdite sauf autorisation expresse du directeur de la publication.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "MentionsLegales",
-  data () {
+  data() {
     return {
       msg: 'Page sur les mentions legales'
     }
@@ -16,5 +74,53 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  font-size: 3.2rem;
+  font-weight: 400;
+  color: black;
+  text-transform: uppercase;
+  padding: 40px;
+  background-color: #F1F1F1;
+  width: 100%;
+}
+
+h1 > span {
+  font-size: 6.4rem;
+  font-weight: 600;
+  background: -webkit-linear-gradient(0deg, rgba(85, 242, 214, 1) 16%, rgba(227, 137, 229, 1) 78%);
+  -webkit-background-clip: text;
+  color: transparent;
+  text-transform: initial;}
+
+div {
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+p{
+  font-family: Lato, sans-serif;
+  font-size: 1.8rem;
+  margin-top: 10px;
+  line-height: 150%;
+}
+p > span {
+  color: gray;
+  font-weight: bold;
+}
+
+h2{
+  font-family: Poppins, sans-serif;
+  font-weight: bolder;
+  font-size: 2.2rem;
+  color: #8D92FF;
+}
+h3{
+  font-family: Lato, sans-serif;
+  font-weight: bold;
+  font-size: 1.8rem;
+  text-decoration: underline black;
+}
 
 </style>
--- src/views/PolitiqueConfidentialite.vue
+++ src/views/PolitiqueConfidentialite.vue
@@ -1,6 +1,105 @@
 <template>
   <div>
-
+    <h1>Politique de <br><span>Confidentialité</span></h1>
+    <div>
+      <p>Dans le cadre de son activité de formation dans l'enseignement supérieur, le Département des Métiers du
+        Multimédia et de l’Internet de Montbéliard, représenté par Alain Lamboux Durand, est amené à collecter et
+        traiter les données personnelles de ses visiteurs. Les activités pour lesquelles la société est amené à traiter
+        des données sensibles sont les suivantes :</p>
+      <ul>
+        <li><p><strong>Analyse statistique de données </strong>: pour étude de marché et reporting.</p></li>
+        <li><p><strong>Réponse aux prises de contact</strong> avec un formulaire présent sur la page « Contact » du site
+          web.</p>
+        </li>
+      </ul>
+      <p>La présente politique de confidentialité a pour vocation de fournir aux internautes une information synthétique
+        et globale sur les traitements de données à caractère personnel opérés par la société de commerce. En accédant à
+        notre site Web et en utilisant nos services, vous reconnaissez avoir lu et compris la présente Politique de
+        confidentialité, ainsi que les pratiques de recueil et de traitement des informations décrites dans ce document.
+        Ce document a été mis à jour pour la dernière fois le 27/11/2021. Alain Lamboux Durand et les responsables du
+        département se réservent le droit de faire évoluer la présente Politique de confidentialité afin de se conformer
+        aux modifications des lois et réglementations en vigueur. Veuillez donc le consulter régulièrement afin de
+        rester informé(e).
+      </p>
+    </div>
+    <div>
+      <h2>Définitions</h2>
+      <p>Le Département MMI accorde une importance particulière au respect de la vie privée des Utilisateurs et de la
+        confidentialité de leurs données personnelles, et s’engage ainsi à traiter les données dans le respect des lois
+        et règlementations applicables, et notamment la Loi n° 78-17 du 6 janvier 1978 relative à l’informatique, aux
+        fichiers et aux libertés (la “Loi Informatique et Liberté”), et le Règlement (UE) 2016/679 du Parlement européen
+        et du Conseil du 27 avril 2016 relatif à la protection des personnes physiques à l’égard du traitement des
+        données à caractère personnel et à la libre circulation de ces données (le “RGPD”).
+      </p>
+      <ul>
+        <li><p><strong>Donnée à caractère personnel </strong> constitue une donnée à caractère personnel toute information
+          relative à une personne physique identifiée ou identifiable, c’est-à-dire qui peut être identifiée,
+          directement ou indirectement, par référence à un numéro d’identification ou à un ou plusieurs éléments qui lui
+          sont propres.
+          </p></li>
+        <li><p><strong>Traitement de données à caractère personnel</strong> : constitue un traitement de données à
+          caractère personnel toute opération portant sur de telles données, quel que soit le procédé utilisé, et
+          notamment la collecte, l’enregistrement, l’organisation, la conservation, l’adaptation ou la modification,
+          l’extraction, la consultation, l’utilisation, la communication par transmission, diffusion ou toute autre
+          forme de mise à disposition, le rapprochement ou l’interconnexion, ainsi que le verrouillage, l’effacement ou
+          la destruction.
+          </p></li>
+        <li><p><strong>Cookie</strong> : un cookie est une information déposée sur le disque dur d’un internaute par le
+          serveur du site qu’il visite. Il contient plusieurs données : le nom du serveur qui l’a déposé, un identifiant
+          sous forme de numéro unique, éventuellement une date d’expiration. Ces informations sont parfois stockées sur
+          l’ordinateur dans un simple fichier texte auquel un serveur accède pour lire et enregistrer des informations.
+        </p></li>
+      </ul>
+    </div>
+    <div>
+      <h2>Responsable légal et responsable du traitement des données</h2>
+      <p>Alain Lamboux Durand
+        <br>Email : alain.lamboux-durand@univ-fcomte.fr
+      </p>
+    </div>
+    <div>
+      <h2>Recueil des informations collectées</h2>
+      <p>Le département MMI recueille des informations vous concernant dans les cas de figure suivants :</p>
+      <h3>Prise de contact volontaire via le formulaire :</h3>
+      <p>Le site du département MMI de Montbéliard propose à ces internautes, via l’intermédiaire d’un formulaire de
+        contact sécurisé, de prendre contact avec le chef de projet, l’équipe et les représentants du département.
+        Ladite prise de contact n’est pas obligatoire pour la bonne consultation du site web, un numéro de téléphone est
+        indiqué.
+      </p>
+      <h3>Protection des mineurs :
+      </h3>
+      <p>Les mineurs ne doivent pas transmettre de données individuelles à des sites Internet à but commercial sans
+        l’autorisation de leurs parents. Le site du département MMI ne collectera en aucun cas délibérément les données
+        individuelles d’enfants, ni ne les utilisera, ni ne les mettra à disposition, ni ne les transmettra à quiconque
+        sans y être autorisé.</p>
+    </div>
+    <div>
+      <h2>Durée de conservation de vos données</h2>
+      <p>Illimité, ou plus précisément nous conserverons vos données tant que vous ne supprimez pas votre compte-client dans le cadre de la vente en ligne. Le site du département MMI sauvegardera vos emails tant que la société utilisera la plateforme Sendinblue pour la diffusion de sa newsletter. En cas de suppression de l’outil, vos informations seront définitivement supprimées de la plateforme.
+        <br>Vous pouvez à tout moment faire appel à votre droit à l’oubli ou à votre droit d’opposition (voir paragraphe suivant).
+      </p>
+    </div>
+    <div>
+      <h2>Cookies</h2>
+      <p>Le site du département MMI de Montbéliard utilise des cookies qui ont pour finalité de faciliter la navigation sur le site, de mesurer l’audience du site ou encore de permettre le partage de pages du site. Les cookies que nous collectons sont les suivants :
+      </p>
+      <ul>
+        <li> <p><strong>Les cookies analytiques</strong> : Ces cookies permettent de connaître l’utilisation et les performances du site et d’en améliorer le fonctionnement en procédant à des analyses de fréquentation des pages d’information, en effectuant un suivi des taux d’ouverture, des taux de clics et des taux de rebond au niveau individuel.
+        </p></li>
+        <li><p><strong>Les cookies de boutons de partage </strong>: Ces cookies sociaux permettent aux utilisateurs de partager des pages et du contenu sur les réseaux sociaux tiers via les boutons sociaux de partage.
+        </p></li>
+      </ul>
+    </div>
+    <div>
+      <h2>Gestion de cookies</h2>
+      <p>Les internautes ont la possibilité d’accepter ou de refuser les cookies sur le site ou de les refuser une fois pour toutes en paramétrant leur navigateur. Si l’internaute choisit de refuser l’ensemble des cookies, la navigation accédant à certaines pages du site sera réduite.
+      </p>
+    </div>
+    <div>
+      <h2>Comment le département MMI de Montbéliard sécurise vos données personnelles ?</h2>
+      <p>Pour les données collectées en ligne, les communications sur la partie cliente sont cryptées entre le poste de l’internaute et nos serveurs (zone sécurisée HTTPS). Les données sont ensuite sauvegardées sur nos serveurs sécurisés hébergés chez le CRI de l’Université de Franche-Comté. Ce fournisseur d’hébergement s’engage à ne pas réutiliser les données personnelles stockées sur les serveurs sécurisés de leurs clients. Le CRI de l’Université de Franche-Comté vous garantit qu’il ne traite pas vos informations en dehors de l’Union européenne ou de tout pays reconnu par la Commission européenne comme disposant d’un niveau de protection des données à caractère personnel suffisant (au regard de la protection de la vie privée, des libertés et droits fondamentaux des personnes, ainsi qu’à l’égard de l’exercice des droits correspondants).
+      </p>
+    </div>
   </div>
 </template>
 
@@ -16,5 +115,51 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  font-size: 3.2rem;
+  font-weight: 400;
+  color: black;
+  text-transform: uppercase;
+  padding: 40px;
+  background-color: #F1F1F1;
+  width: 100%;
+}
+
+span {
+  font-size: 6.4rem;
+  font-weight: 600;
+  background: -webkit-linear-gradient(0deg, rgba(85, 242, 214, 1) 16%, rgba(227, 137, 229, 1) 78%);
+  -webkit-background-clip: text;
+  color: transparent;
+  text-transform: initial;}
+
+div {
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+p{
+  font-family: Lato, sans-serif;
+  font-size: 1.8rem;
+  margin-top: 10px;
+  line-height: 150%;
+}
+strong {
+  text-decoration: underline black;
+}
+h2{
+  font-family: Poppins, sans-serif;
+  font-weight: bolder;
+  font-size: 2.2rem;
+  color: #8D92FF;
+}
+h3{
+  font-family: Lato, sans-serif;
+  font-weight: bold;
+  font-size: 1.8rem;
+  text-decoration: underline black;
+}
 
 </style>
